1f64f56c00bff039dd8a5572aa2c2811c9b9a725
System Administrator <admin@example.com>
2019-05-07 14:50:23 +0000

    Changes by 'System Administrator' on 'Default' -- Build #1

---
+++ sn/GIT PR application/Dictionary Entry/x_317910_git_pr_ap_git_pr_application_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : GIT PR application
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-05-07 14:46:35
+ * Created By  : admin
+ * Updated On  : 2019-05-07 14:46:35
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=85c8bb0ddb153300c9fe7b3868961982
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
